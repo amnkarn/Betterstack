19d874e4e1eef889beea520e73debc12012f9bfd
Aman Kumar Karn <amankarn2007@gmail.com>
2026-07-27 10:21:30 +0530

    user signin and signup endpoint test working

--- apps/api/src/validator/index.ts
+++ apps/api/src/validator/index.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const UserInputSchema = z.object({
-    username: z.string().min(3).max(20),
-    password: z.string().min(3).max(50),
+    username: z.string().min(3),
+    password: z.string().min(3),
 })
 
 export const CreateWebsiteInput = z.object({
--- apps/test/user.test.ts
+++ apps/test/user.test.ts
@@ -1,103 +1,91 @@
-import axios from "axios";
+import axios2 from "axios";
 import { describe, expect, it, test } from "bun:test";
 
 const BASE_URL = "http://localhost:3000";
 
-describe("Signup endpoint", () => {
-    test("Isnt able to sign up if body is incorrect", async () => {
-        try {
-            const res = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
-                username: "aman" + Math.random(),
-                password: "password"
-            })
+const axios = axios2.create({ //to accept all status code
+    validateStatus: () => true,
+})
 
-            expect(res.status).toBe(403);
-            expect(false, "Control should not reach here");
-        } catch (error) { }
+describe("Signup endpoint", () => {
+    test("Isn't able to sign up if body is incorrect", async () => {
+        const res = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
+            email: "aman" + Math.random(),
+            password: "password"
+        })
+
+        expect(res.status).toBe(403);
+        expect(false, "Control should not reach here");
     })
 
     test("Is able to sign up if body is correct", async () => {
-        try {
-            const res = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
-                username: "aman" + Math.random(),
-                password: "password"
-            })
-
-            expect(res.status).toBe(201);
-            expect(res.data.id).toBeDefined();
-        } catch (error) {
-            
-        }
+        const res = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
+            username: "aman" + Math.floor(Math.random() * 1000),
+            password: "password"
+        })
+        console.log(res.data.message);
+        expect(res.status).toBe(201);
+        expect(res.data.userId).toBeDefined();
     })
 
     test('User is able to sign up only once', async () => {
-        const username = "aman" + Math.random();
+        const username = "aman" + Math.floor(Math.random() * 1000);
         const password = "123456";
 
-        try {
-            const response = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
-                username,
-                password,
-            })
+        const response = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
+            username,
+            password,
+        })
 
-            expect(response.status).toBe(201);
-            expect(response.data.id).toBeDefined();
+        expect(response.status).toBe(201);
+        expect(response.data.userId).toBeDefined();
 
-            const updatedRequest = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
-                username,
-                password,
-            })
+        const updatedRequest = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
+            username,
+            password,
+        })
 
-            expect(updatedRequest.status).toBe(400)   
-        } catch (error) {
-            
-        }
+        expect(updatedRequest.status).toBe(400)
     })
 })
 
 describe("Signin endpoint", () => {
     test("Isn't able to sign in if body is incorrect", async () => {
-        try {
-            const username = "aman" + Math.random();
-            
-            const res = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
-                username: username,
-                password: "password"
-            })
+        const username = "aman" + Math.random();
 
-            await axios.post(`${BASE_URL}/user/signin`, {
-                email: username,
-                password: "password"
-            })
-            expect(false, "Control shouldnt reach here")
-        
-        } catch(e) {
-        
-        }
+        const res = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
+            username: username,
+            password: "password"
+        })
+
+        expect(res.status).toBe(201);
+        expect(res.data.userId).toBeDefined();
+
+        const signup = await axios.post(`${BASE_URL}/api/v1/user/signin`, {
+            email: username,
+            password: "password"
+        })
+
+        expect(signup.status).toBe(403);
     })
 
     test("Signin succeeds if username and password are correct", async () => {
         const username = "aman" + Math.random();
 
-        try {
-            const res = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
-                username: username,
-                password: "password"
-            })
+        const res = await axios.post(`${BASE_URL}/api/v1/user/signup`, {
+            username: username,
+            password: "password"
+        })
 
-            expect(res.status).toBe(201);
-            expect(res.data.id).toBeDefined();
+        expect(res.status).toBe(201);
+        expect(res.data.userId).toBeDefined();
 
-            const signin = await axios.post(`${BASE_URL}/api/v1/user/signin`, {
-                username: username,
-                password: "password"
-            })
+        const signin = await axios.post(`${BASE_URL}/api/v1/user/signin`, {
+            username: username,
+            password: "password"
+        })
 
-            expect(signin.status).toBe(200);
-            expect
-
-        } catch (error) { }
+        expect(signin.status).toBe(200);
+        expect(signin.data.token).toBeDefined();
     })
-
-   
 })
